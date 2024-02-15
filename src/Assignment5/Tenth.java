63e40b01669b99c87fa7582cae3ec954972cfa94
MaanLad <maanlad25@gmail.com>
2024-02-15 13:19:00 +0530

    tenth file added

--- src/Assignment5/Tenth.java
+++ src/Assignment5/Tenth.java
@@ -1,6 +1,8 @@
 package Assignment5;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tenth {
 
@@ -12,12 +14,55 @@ public class Tenth {
         Arrays.sort(arr2);
         return Arrays.equals(arr1,arr2);
     }
-    public static void main(String[] args) {
+    public static ArrayList<String> newList=new ArrayList<String>();
 
-        //Calling the method with two string
-        System.out.println(checkAnagram("maan","aanm"));
+    public static boolean hasValue(String str){
 
-
-
+        if(newList.contains(str)){
+            return false;
+        }
+        else{
+            newList.add(str);
+            return true;
+        }
     }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>>  list= new ArrayList<List<String>>();
+
+        for (int i = 0; i < strs.length; i++) {
+            List<String> sublist=new ArrayList<String>();
+            char[] arr=strs[i].toCharArray();
+            char[] arr=strs[i].toCharArray();
+            sublist.add(strs[i]);
+            Arrays.sort(arr);
+            if(hasValue(Arrays.toString(arr))){
+                for (int j = 0; j < strs.length; j++) {
+                    char[] arr2=strs[j].toCharArray();
+                    Arrays.sort(arr2);
+                    if(i!=j && Arrays.equals(arr2,arr)){
+                        sublist.add(strs[j]);
+                    }
+                }
+                list.add(sublist);
+            }
+
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        //Calling the method with two string
+        System.out.println("Check anagram :-"+checkAnagram("maan","aanm"));
+
+        String[] strs = {"art","rat","tar","meats","steam"};
+        System.out.println();
+        System.out.println("Input array :- "+Arrays.toString(strs));
+        System.out.println("Output :- ");
+        List<List<String>> lst=groupAnagrams(strs);
+        for (int i = 0; i < lst.size(); i++) {
+            System.out.println(lst.get(i));
+        }
+    }
+
 }

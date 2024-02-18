7e1e9558805e4b7f3d3dc3aed48794499465aba1
MaanLad <maanlad25@gmail.com>
2024-02-18 17:21:25 +0530

    Database programm added

---
+++ src/Assignment6/Fourth.java
@@ -0,0 +1,29 @@
+package Assignment6;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Fourth {
+    public static void main(String[] args) {
+        String sampleSqlQuery="select * from student";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
+            if(conn!=null){
+                System.out.println("Database is connected");
+                Statement st=conn.createStatement();
+                ResultSet rs=st.executeQuery(sampleSqlQuery);
+                while(rs.next()){
+                    System.out.print(rs.getInt(1)+"\t");
+                    System.out.print(rs.getString(2)+"\t");
+                }
+            }
+
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+            System.out.println("Not connected");
+        }
+    }
+}

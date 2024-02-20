a5b5632d7a8449846c7d4b7047264a08aac48b81
Maan Lad <109408213+MaanLad@users.noreply.github.com>
2024-02-20 17:38:09 +0530

    Update LongestCommonSequence.java
    Changes added error while single input

--- src/Others/LongestCommonSequence.java
+++ src/Others/LongestCommonSequence.java
@@ -8,14 +8,14 @@ public class LongestCommonSequence {
     public static void main(String[] args) {
         int[] nums={100,4,200,1,3,2};
         Arrays.sort(nums);
-        ArrayList<Integer> arrList=new ArrayList<>();
+        ArrayList<Integer> arrlist=new ArrayList<>();
             for (int i = 0; i <nums.length ; i++) {
-            arrList.add(nums[i]);
+            arrlist.add(nums[i]);
         }
             int result=0;
             int count=0;
             int i=0;
-//        List<Integer> arrList = arrList.stream().distinct().toList();
+        List<Integer> arrlist = arrList.stream().distinct().toList();
 //        System.out.println(arrList.get(count)+arrList.get(count+1)+1);
 
             while( i<arrList.size()){

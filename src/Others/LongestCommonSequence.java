b5261f7f682ab9be992e77f23e2380f7248ebdb9
MaanLad <maanlad25@gmail.com>
2024-02-20 19:24:59 +0530

    final LCS leet code files

--- src/Others/LongestCommonSequence.java
+++ src/Others/LongestCommonSequence.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class LongestCommonSequence {
     public static void main(String[] args) {
         int[] nums={100,4,200,1,3,2};
+//        int[] nums={0,-1};
+//        int[] nums={9,1,4,7,3,-1,0,5,8,-1,6};
+//        int[] nums={-8,-2,-3,-9,-6,7,9,-8,9,2,-8};
         Arrays.sort(nums);
         ArrayList<Integer> arrlist=new ArrayList<>();
             for (int i = 0; i <nums.length ; i++) {
@@ -15,22 +18,22 @@ public class LongestCommonSequence {
             int result=1;
             int count=0;
             int i=0;
-        List<Integer> arrlist = arrList.stream().distinct().toList();
-//        System.out.println(arrList.get(count)+arrList.get(count+1)+1);
-
-            while( i<arrList.size()){
-                if(arrList.get(count)+1 == arrList.get(count + 1)){
+        List<Integer> arrList = arrlist.stream().distinct().toList();
+        System.out.println(arrList);
+        System.out.println(arrList);
+            while( i<arrList.size()-1){
+                if(arrList.get(i)+1 == arrList.get(i + 1)){
                     count++;
                 }else{
-                    if(count>result){
-                        result=count;
+                    if(count>=result){
+                        result=count+1;
                     }
                     count=0;
                 }
                 i++;
             }
-            if(count>result){
-                result=count;
+            if(count>=result){
+                result=count+1;
             }
         System.out.println(result);
     }
